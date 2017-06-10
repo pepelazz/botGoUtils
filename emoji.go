d51b1afbb269fa0af20941b9ec1bdbce4fbf1100
Trikster <trikster@MBP-MacBookPro.Dlink>
2017-06-10 19:47:00 +0300

    добавил эмоджи

---
+++ emoji.go
@@ -0,0 +1,35 @@
+package goBotUtils
+
+// http://apps.timwhitlock.info/emoji/tables/unicode
+var Emoji = struct {
+	ThumbUp           string
+	ThumbDown         string
+	Hourglass         string
+	Silhouette        string
+	CheqFlag          string
+	ClosedLockWithKey string
+	LeftBlackArrow    string
+	Key               string
+	Ledger            string
+	Newspaper         string
+	Books             string
+	Watch             string
+	Trophy            string
+	Eyeglasses        string
+}{
+	"\U0001F44D", // ThumbUp
+	"\U0001F44E", // ThumbDown
+	"\U0000231B", // Hourglass
+	"\U0001F464", // Silhouette
+	"\U0001F3C1", // CheqFlag
+	"\U0001F510", // ClosedLockWithKey
+	"\U00002B05", // LeftBlackArrow
+	"\U0001F511", // Key
+	"\U0001F4D2", // Ledger
+	"\U0001F4F0", // Newspaper
+	"\U0001F4DA", // Books
+	"\U0000231A", // Watch
+	"\U0001F3C6", // Trophy
+	"\U0001F453", // Eyeglasses
+}
+
